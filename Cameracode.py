446fb0787cc25ba01042b0a60c283e285737945e
MahadiAlif <90108275+MahadiAlif@users.noreply.github.com>
2023-11-26 14:55:48 +0100

    Update Cameracode.py

--- Cameracode.py
+++ Cameracode.py
@@ -1,1 +1,17 @@
+from picamera import PiCamera
+from time import sleep
+
+# Initialize the camera
+camera = PiCamera()
+
+# Set the resolution (optional)
+camera.resolution = (1024, 768)
+
+# Capture an image
+camera.capture('imageT.jpg')
+
+
+
+# Close the camera
+camera.close()
 
